f8ac69ed85956ae25da22fed8241e3b08cc630c4
AndreFirstOrDefault <andre.java.ads.2022@gmail.com>
2024-06-11 18:29:16 -0300

    Criação dos projetos

---
+++ CustomerRegistry.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomerRegistry.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CustomerRegistry.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomerRegistry.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
